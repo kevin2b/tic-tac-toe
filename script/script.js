cdf0851d7a9d28feedca5603480c833f18bb4c1f
kevin2b <25062561+kevin2b@users.noreply.github.com>
2023-11-22 14:03:54 -0500

    Add tic-tac-toe logic in JS

--- script/script.js
+++ script/script.js
@@ -1,55 +1,116 @@
 
-function GameBoard(){
-  const board = [];
-  const SIZE = 3;
-  
-  for (let r = 0; r < SIZE; r++){
-    const row = [];
-    for (let c = 0; c < SIZE; c++){
-      row.push(Cell());
-    }
-    board.push(row);
-  }
-  
-  function displayBoard(){
-    const output = board.map((row) => row.map((entry) => entry.getContent()));
-    console.log(output);
-  }
-  
-  //Row starts from top, column starts from left. (Top left is 0, 0);
-  function setBoard(row, col, value){
-    if (row >= 0 && row < SIZE && col >= 0 && col < SIZE){
-      board[row][col].setContent(value);
-    }
-  }
-  
-  //Returns true if marker exist SIZE times in a row, column or diagonal
-  function foundWinner (marker){
-    //Check winner in row or column
-    for (let i = 0; i < SIZE; i++){
-      let rowWin = true;
-      let colWin = true;
-      for (let j = 0; j < SIZE; j++){
-          rowWin = rowWin && board[i][j].getContent() === marker;
-          colWin = colWin && board[j][i].getContent() === marker;
-      }
-      if (rowWin || colWin){
-        return true;
+function GameSession(){
+  const gameBoard = (function (){
+    var board = [];
+    const SIZE = 3;
+    resetBoard();
+    
+    function resetBoard (){
+      board = [];
+      for (let r = 0; r < SIZE; r++){
+        const row = [];
+        for (let c = 0; c < SIZE; c++){
+          row.push(Cell());
+        }
+        board.push(row);
       }
     }
     
-    //Check winner in diagonal
-    let diaDownWin = true;
-    let diaUpWin = true;
-    for (let i = 0; i < SIZE; i++){
-      diaDownWin = diaDownWin && board[i][i].getContent() === marker;
-      diaUpWin = diaUpWin && board[SIZE-i-1][i].getContent() === marker;
+    function displayBoard(){
+      const output = board.map((row) => row.map((entry) => entry.getContent()));
+      console.log(output);
     }
-    return diaDownWin || diaUpWin;
+    
+    //Row starts from top, column starts from left. (Top left is 0, 0). Return true if successful;
+    function setBoard(row, col, value){
+      if (row >= 0 && row < SIZE && col >= 0 && col < SIZE &&  board[row][col].getContent() === " "){
+        board[row][col].setContent(value);
+        return true;
+      }
+      return false;
+    }
+    
+    //Returns true if marker exist SIZE times in a row, column or diagonal
+    function foundWinner (marker){
+      //Check winner in row or column
+      for (let i = 0; i < SIZE; i++){
+        let rowWin = true;
+        let colWin = true;
+        for (let j = 0; j < SIZE; j++){
+            rowWin = rowWin && board[i][j].getContent() === marker;
+            colWin = colWin && board[j][i].getContent() === marker;
+        }
+        if (rowWin || colWin){
+          return true;
+        }
+      }
+      
+      //Check winner in diagonal
+      let diaDownWin = true;
+      let diaUpWin = true;
+      for (let i = 0; i < SIZE; i++){
+        diaDownWin = diaDownWin && board[i][i].getContent() === marker;
+        diaUpWin = diaUpWin && board[SIZE-i-1][i].getContent() === marker;
+      }
+      return diaDownWin || diaUpWin;
+    }
+    
+    return {displayBoard, setBoard, foundWinner, resetBoard};
+  })();
+  
+  const PLAYER_1_NAME = "Player 1";
+  const PLAYER_2_NAME = "Player 2";
+  const PLAYER_1_MARKER = "O";
+  const PLAYER_2_MARKER = "X";
+  let firstPlayerTurn = true;
+  startGame();
+  
+  function startGame(){
+    gameBoard.resetBoard();
+    console.log(`${PLAYER_1_NAME}'s turn.`);
+    gameBoard.displayBoard();
   }
   
-  return {displayBoard, setBoard, foundWinner};
+  function playRound(row, col){
+    if (gameBoard.foundWinner(PLAYER_1_MARKER) || gameBoard.foundWinner(PLAYER_2_MARKER)){
+      console.log("Please start a new game");
+      return;
+    }
+    
+    if (firstPlayerTurn){
+      const validMove = gameBoard.setBoard(row, col, PLAYER_1_MARKER);
+      if (validMove && gameBoard.foundWinner(PLAYER_1_MARKER)){
+        console.log(`${PLAYER_1_NAME} wins!`);
+        gameBoard.displayBoard();
+        return;
+      }
+      if (validMove){
+        console.log(`${PLAYER_2_NAME}'s turn.`);
+        gameBoard.displayBoard();
+        firstPlayerTurn = !firstPlayerTurn;
+        return;
+      }
+    }
+    
+    if (!firstPlayerTurn){
+      const validMove = gameBoard.setBoard(row, col, PLAYER_2_MARKER);
+      if (validMove && gameBoard.foundWinner(PLAYER_2_MARKER)){
+        gameBoard.displayBoard();
+        console.log(`${PLAYER_2_NAME} wins!`);
+        return;
+      }
+      if (validMove){
+        console.log(`${PLAYER_1_NAME}'s turn.`);
+        gameBoard.displayBoard();
+        firstPlayerTurn = !firstPlayerTurn;
+        return;
+      }
+    }
+  }
+
+  return {startGame, playRound}; 
 }
+
 
 function Cell(){
   let content = " ";
@@ -62,4 +123,4 @@ function Cell(){
 
 
 
-const gameboard = GameBoard();
+const g = GameSession();
